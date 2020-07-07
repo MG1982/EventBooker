50d1e1d2f3bbca79008f9b50b028ebc7c9b4d1bf
Mani <manigonzalez82@gmail.com>
2020-07-07 20:07:33 +0800

    MongoDB
    Added MongoDB ad Mongoose to the project.
    User and event models created.

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const Event = require("./models/event");
+const User = require("./models/user");
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -21,6 +24,12 @@ app.use(
             date: String!
         }
 
+        type User {
+            _id: ID!
+            email: String!
+            password: String
+        }
+
         input EventInput {
             title: String!
             description: String!
@@ -28,11 +37,17 @@ app.use(
             date: String!
         }
 
+        input UserInput {
+            email: String!
+            password: String!
+        }
+
         type RootQuery {
             events: [Event!]!
         }
         type RootMutation {
             createEvent(eventInput: EventInput): Event
+            createUser(userInput: UserInput): User
         }
         schema {
             query: RootQuery
@@ -41,22 +56,75 @@ app.use(
     `),
         rootValue: {
             events: () => {
-                return events;
+                return Event.find()
+                    .then(events => {
+                        return events.map(event => {
+                            return { ...event._doc }
+                        })
+                    }).catch(err => {
+                        throw err;
+                    })
             },
             createEvent: (args) => {
-                const event = {
-                    _id: Math.random().toString(),
+                const event = new Event({
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: args.eventInput.date
-                }
-                events.push(event);
-                return event;
+                    date: new Date(args.eventInput.date),
+                    creator: "5f045e0d9fe03a50e4a60d7c"
+                });
+                let createdEvent;
+                return event
+                    .save()
+                    .then(result => {
+                        createdEvent = { ...result._doc };
+                        return User.findById("5f045e0d9fe03a50e4a60d7c")
+                    })
+                    .then(user => {
+                        if (!user) {
+                            throw new Error("User not found.");
+                        }
+                        user.createdEvents.push(event);
+                        return user.save();
+                    })
+                    .then(result => {
+                        return createdEvent;
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        throw err;
+                    });
+            },
+            createUser: args => {
+                return User.findOne({ email: args.userInput.email }).then(user => {
+                    if (user) {
+                        throw new Error("User exists already.")
+                    }
+                    return bcrypt
+                        .hash(args.userInput.password, 12)
+                })
+                    .then(hashedPassword => {
+                        const user = new User({
+                            email: args.userInput.email,
+                            password: hashedPassword
+                        });
+                        return user.save();
+                    })
+                    .then(result => {
+                        return { ...result._doc, password: null }
+                    })
+                    .catch(err => {
+                        throw err;
+                    })
             }
         },
         graphiql: true
     })
 );
+mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-tu3ej.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`).then(() => {
+    app.listen(3000);
+})
+    .catch(err => {
+        console.log(error);
+    });
 
-app.listen(3000);
