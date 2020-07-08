59d00c7b74f94036743fed250aa838b116c635ae
Mani <manigonzalez82@gmail.com>
2020-07-08 20:39:27 +0800

    Simplified app.js
    Moved GraphQL Resolvers and Schema to their own folders to clean up app.js.
    Added async/await to resolvers.

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,101 @@
+const bcrypt = require("bcryptjs");
+const Event = require("../../models/event");
+const User = require("../../models/user");
+
+
+const events = async eventIds => {
+    const events = await Event.find({ _id: { $in: eventIds } })
+    try {
+        events.map(event => {
+            return {
+                ...event._doc,
+                _id: event.id,
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, event.creator),
+            }
+        });
+        return events;
+    } catch (err) {
+        throw err;
+    }
+};
+
+const user = async userId => {
+    const user = await User.findById(userId)
+    try {
+        return {
+            ...user._doc,
+            _id: user.id,
+            createdEvents: events.bind(this, user._doc.createdEvents)
+        };
+    } catch (err) {
+        throw err;
+    }
+}
+
+module.exports = {
+    events: async () => {
+        const events = await Event.find()
+        try {
+            return events.map(event => {
+                return {
+                    ...event._doc,
+                    _id: event.id,
+                    date: new Date(event._doc.date).toISOString(),
+                    creator: user.bind(this, event._doc.creator)
+                }
+            });
+        } catch (err) {
+            throw err;
+        }
+    },
+    createEvent: async args => {
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date),
+            creator: "5f045e0d9fe03a50e4a60d7c"
+        });
+        let createdEvent;
+        try {
+            const result = await event.save()
+            createdEvent = {
+                ...result._doc,
+                _id: result._doc._id.toString(),
+                date: new Date(event._doc.date).toISOString(),
+                creator: user.bind(this, result._doc.creator)
+            };
+            const creator = await User.findById("5f045e0d9fe03a50e4a60d7c")
+
+            if (!creator) {
+                throw new Error("User not found.");
+            }
+            creator.createdEvents.push(event);
+            await creator.save();
+            return createdEvent;
+        } catch (err) {
+            throw err;
+        }
+    },
+    createUser: async args => {
+        try {
+            const existingUser = await User.findOne({ email: args.userInput.email })
+            if (existingUser) {
+                throw new Error("User exists already.")
+            }
+            const hashedPassword = await bcrypt
+                .hash(args.userInput.password, 12)
+
+            const user = new User({
+                email: args.userInput.email,
+                password: hashedPassword
+            })
+            const result = await user.save();
+
+            return { ...result._doc, password: null }
+        } catch (err) {
+            throw err;
+        }
+    }
+}
