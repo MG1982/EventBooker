0835fc993dde91bc982b94f3691f5aade67f31df
Mani <manigonzalez82@gmail.com>
2020-07-14 19:09:28 +0800

    Pages
    Basic page components and routes setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Redirect, Switch } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>It Works!</h1>
-    </div>
-  );
+import AuthPage from "./pages/Auth";
+import EventsPage from "./pages/Events";
+import BookingsPage from "./pages/Bookings";
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Redirect from="/" to="/auth" exact />
+          <Route path="/auth" component={AuthPage} />
+          <Route path="/events" component={EventsPage} />
+          <Route path="/bookings" component={BookingsPage} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
