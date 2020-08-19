02087ed38ad7d5a6876e5538e3cadee2f6cd5b34
Mani <manigonzalez82@gmail.com>
2020-08-19 20:03:12 +0800

    Chartjs
    added chartjs and created a chart to show 3 types of bookings.

--- frontend/src/pages/Bookings.js
+++ frontend/src/pages/Bookings.js
@@ -4,6 +4,7 @@ import AuthContext from "../context/auth-context";
 import Spinner from "../components/Spinner/Spinner";
 import BookingList from "../components/Bookings/BookingList/BookingList";
 import BookingsChart from "../components/Bookings/BookingsChart/BookingsChart";
+import BookingsControls from "../components/Bookings/BookingsControls/BookingsControls";
 
 class BookingsPage extends Component {
     state = {
@@ -30,6 +31,7 @@ class BookingsPage extends Component {
                   _id
                   title
                   date
+                  price
               }
             }
           }
@@ -117,10 +119,8 @@ class BookingsPage extends Component {
         if (!this.state.isLoading) {
             content = (
                 <React.Fragment>
-                    <div>
-                        <button onClick={this.changeOutputTypeHandler.bind(this, "list")}>List</button>
-                        <button onClick={this.changeOutputTypeHandler.bind(this, "chart")}>Chart</button>
-                    </div>
+                    <BookingsControls activeOutputType={this.state.outputType}
+                        onChange={this.changeOutputTypeHandler} />
                     <div>
                         {this.state.outputType === "list" ? (<BookingList bookings={this.state.bookings} onDelete={this.deleteBookingHandler} />) : (<BookingsChart bookings={this.state.bookings} />)}
                     </div>
